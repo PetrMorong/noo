47a019aa9b607420aace7eb24961e7399856710f
Petr Morong <moriandr73@gmail.com>
2018-01-27 17:25:08 +0100

    small-fixes

--- app/components/Sidemenu/SidemenuHeader.js
+++ app/components/Sidemenu/SidemenuHeader.js
@@ -6,10 +6,8 @@ export default styled.div`
   height: 59px;
   border-bottom: 1px solid #EEEEEE;
   margin-bottom: 15px;
-  margin-left: 65px;
   justify-content: center;
-
   @media (max-width: 768px) {
-    margin-left: 25px;
+    display: none;
   }
 `;
